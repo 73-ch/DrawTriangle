5eeb5b5cc2c26a9430a11d5bb5f251ffa6585dcc
nami634 <cigarbox.nami@gmail.com>
2018-06-22 20:38:47 +0900

    Vulkan Tutorial - Base codeの最初のコード

--- DrawTriangle/main.cpp
+++ DrawTriangle/main.cpp
@@ -6,10 +6,48 @@
 //  Copyright © 2018年 Kousaku Namikawa. All rights reserved.
 //
 
+// errorやlog用
 #include <iostream>
+#include <stdexcept>
+// lambda function用　主にresorce management
+#include <functional>
+// EXIT_SUCCESSとEXIT_FAILUREを提供する
+#include <cstdlib>
+// 3DCG ライブラリ
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+    
+private:
+    void initVulkan() {
+        
+    }
+    
+    void mainLoop() {
+        
+    }
+    
+    void cleanup() {
+        
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    HelloTriangleApplication app;
+    
+    try {
+        app.run();
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
 }
